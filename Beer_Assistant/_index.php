8d14334a35b5529add522785cd671a615aeafadf
vince <vince@DESKTOP-T101K01>
2017-11-10 15:36:34 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -22,6 +22,7 @@
     
     <div id="chartdiv" style="height:400px; width:300px; "></div>
     
+    
     <script type="text/javascript">
     $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]],
 	{ title:'Exponential Line',
