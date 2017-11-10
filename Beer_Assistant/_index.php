1c3d708e56d700c3e5535de499cc612a3b5ae220
vince <vince@DESKTOP-T101K01>
2017-11-10 15:30:21 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -18,9 +18,10 @@
     <!--  Plotting graph -->
     <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
 	<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />   
+	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
     
-    <div id="chartdiv" style="height:400px;width:300px; "></div>
+    <div id="chartdiv" style="height:400px; width:300px; "></div>
+    
     <script type="text/javascript">
     $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]],
 	{ title:'Exponential Line',
