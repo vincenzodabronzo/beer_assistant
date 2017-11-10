90888b6d20fdc7aff6ab031da8edd49b1db96f97
vince <vince@DESKTOP-T101K01>
2017-11-10 16:12:35 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -2,27 +2,80 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<!--[if lt IE 9]>
-        <script src="/js/html5shiv.js"></script>
-    <![endif]-->
+	<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
+    
     <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
+    
     <script type="text/javascript">
 		$(document).ready(function() {
 			setInterval(function() {
 				$('#show').load('lib/data.php');
 			}, 1000 );
+			
+			var t = 2000;
+			var x = (new Date()).getTime(); // current time
+			var n = 20;
+			data = [];
+			for(i=0; i&lt;n; i++){  
+			    data.push([x - (n-1-i)*t,0]);  
+			}   
+			var options = {      
+			      axes: {   	    
+			         xaxis: {   	   	   
+			            numberTicks: 4,            
+			            renderer:$.jqplot.DateAxisRenderer,           
+			            tickOptions:{formatString:'%H:%M:%S'},            
+			            min : data[0][0],           
+			            max: data[data.length-1][0] 	   
+			         }, 	    
+			         yaxis: {
+			            min:0, 
+			            max: 1,
+			            numberTicks: 6,   	        
+			            tickOptions:{formatString:'%.1f'}  	    
+			         }      
+			      },      
+			      seriesDefaults: {   	    
+			         rendererOptions: { smooth: true}      
+			      }  
+			   };  
+			 
+			   var plot1 = $.jqplot ('myChart', [data],options); 
+			 
+			   $('button').click( function(){        
+			      doUpdate();      
+			      $(this).hide();  
+			   });
+			 
+			   function doUpdate() {      
+			      if(data.length &gt; n-1){
+			         data.shift();
+			      }
+			      var y = Math.random();
+			      var x = (new Date()).getTime();
+			      data.push([x,y]);
+			      if (plot1) {
+			    	plot1.destroy();
+			      }
+			      plot1.series[0].data = data; 
+			      options.axes.xaxis.min = data[0][0];
+			      options.axes.xaxis.max = data[data.length-1][0];
+			      plot1 = $.jqplot ('myChart', [data],options);
+			      setTimeout(doUpdate, t);
+			   }
+
+			
 		});
     </script>
     
     
-    <!--  Plotting graph -->
-    <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
-	<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
+    <div id="myChart" style="height:400px; width:300px; "></div>
     
+    <!--  Chart Example 2
     <div id="chartdiv" style="height:400px; width:300px; "></div>
-    
-    
     <script type="text/javascript">
     $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]],
 	{ title:'Exponential Line',
@@ -30,7 +83,7 @@
  		 series:[{color:'#5FAB78'}]
 	});
     </script>
-    
+    -->
     
 	<title>Mashing Temperature</title>
 </head>
