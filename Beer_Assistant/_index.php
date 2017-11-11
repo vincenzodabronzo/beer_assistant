e5a9f6d6a3a3b8d80fbc44134014f9c7214bf468
vince <vince@DESKTOP-T101K01>
2017-11-11 11:56:21 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -96,6 +96,11 @@
 				function doUpdate() {
 					if (update_graph) {
 
+						var loc = window.location.pathname;
+						var dir = loc.substring(0, loc.lastIndexOf('/'));
+
+						alert(dir);
+						
 						$('#show').load('lib/data.php');
 
 						$.ajax({
