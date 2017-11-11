0007db6a579a3437cb999c51ca972809647d9b07
vince <vince@DESKTOP-T101K01>
2017-11-11 12:10:48 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -103,7 +103,7 @@
 						$('#show').load('lib/data.php');
 
 						$.ajax({
-							   url: 'beer_assistant/Beer_Assistant/py/temp_mashing_nosensor_v1_3.py'
+							   url: 'py/temp_mashing_nosensor_v1_3.py'
 							   // success: function(response) {
 							     // here you do whatever you want with the response variable
 							   // }
