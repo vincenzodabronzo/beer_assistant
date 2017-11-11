739df46712a40596db50f3bf88ebc0281ae5415d
vince <vince@DESKTOP-T101K01>
2017-11-11 13:18:36 +0100

    0.1.0a

--- Beer_Assistant/lib/data.php
+++ Beer_Assistant/lib/data.php
@@ -1,7 +1,7 @@
 <?php
     ini_set('display_errors', 'On');
 
-    shell_exec("python ../py/temp_mashing_nosensor_v1_3.py &");
+    shell_exec("python ../py/temp_mashing_nosensor_v1_3.py");
 
     $dbconn =  new mysqli('localhost', 'pi', 'raspberry', 'dbeer');
     if($dbconn->connect_error) {
