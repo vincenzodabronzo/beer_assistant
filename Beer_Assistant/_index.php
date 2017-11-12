7f393bc4bcde14778ca56e06148c2db5f7f3b95f
vince <vince@DESKTOP-T101K01>
2017-11-12 12:24:23 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -115,7 +115,7 @@
 
 						var ts = $('#current_timestamp').text().split(/[- :]/);
         				// Apply each element to the Date function
-       					var x = ( new Date(Date.UTC(ts[0], ts[1]-1, ts[2], ts[3], ts[4], ts[5])) ).getTime();
+       					var x = ( new Date(Date.UTC(ts[0], ts[1], ts[2], ts[3], ts[4], ts[5])) ).getTime();
     					
     					
     					data.push([x,y]);
