b7e6a65950aa7d2fb8fb79f15f270b7dacdc92ab
vince <vince@DESKTOP-T101K01>
2017-11-12 12:04:23 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -91,6 +91,11 @@
 					$(this).hide();
 					$('#start').show();
 				});
+
+				Date.prototype.addHours = function(h) {    
+					   this.setTime(this.getTime() + (h*60*60*1000)); 
+					   return this;   
+					}
 			   
 			 
 				function doUpdate() {
