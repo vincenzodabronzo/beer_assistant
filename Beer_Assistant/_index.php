1bfc0b86dc2b94229bc34d74dd77a4c5d20b8d43
vince <vince@DESKTOP-T101K01>
2017-11-12 12:30:59 +0100

    0.1.0a

--- Beer_Assistant/_index.php
+++ Beer_Assistant/_index.php
@@ -116,6 +116,7 @@
 						var ts = $('#current_timestamp').text().split(/[- :]/);
         				// Apply each element to the Date function
        					var x = ( new Date(Date.UTC(ts[0], ts[1], ts[2], ts[3], ts[4], ts[5])) ).getTime();
+       					x = x - 3600000; // Timezone: subtract 1 Hour
     					
     					
     					data.push([x,y]);
