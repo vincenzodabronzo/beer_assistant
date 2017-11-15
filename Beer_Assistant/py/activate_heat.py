5a9deec1939cc705fefc5eed5a3bbc989f9a5ed6
vince <vince@DESKTOP-T101K01>
2017-11-15 19:03:30 +0100

    0.1.0a

--- Beer_Assistant/py/activate_heat.py
+++ Beer_Assistant/py/activate_heat.py
@@ -2,7 +2,16 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+# init list with pin numbers
+
+pinList = [17, 27]
+
 # loop through pins and set mode and state to 'low'
 
+GPIO.setup(17, GPIO.OUT) 
 GPIO.output(17, GPIO.HIGH)
+GPIO.setup(27, GPIO.OUT) 
 GPIO.output(27, GPIO.HIGH)
+
+GPIO.output(17, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
