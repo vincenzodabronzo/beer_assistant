eac44302f9881f36c75a05c002bbbed87e0eeddf
vince <vince@DESKTOP-T101K01>
2017-11-15 16:51:56 +0100

    0.1.0a

---
+++ Beer_Assistant/py/temp_mashing_v1_0.py
@@ -0,0 +1,55 @@
+'''
+Created on 8 Nov 2017
+
+@author: kenzo
+'''
+
+import random
+import MySQLdb
+import time
+from w1thermsensor import W1ThermSensor 
+
+# import argparse
+ 
+# Variables for MySQL
+db = MySQLdb.connect(host="localhost", user="pi", passwd="raspberry", db="dbeer")
+cur = db.cursor()
+
+#parser = argparse.ArgumentParser()
+#parser.add_argument("id", help="give batch id", type=int)
+#args = parser.parse_args()
+#id = args.id
+
+id = 2 # id batch - check if ending_time != null
+interval = 1 # sec waiting
+
+sensor = W1ThermSensor()  
+ 
+def getTemp():
+    #temp_c = random.randint(0,100)
+    temperature = sensor.get_temperature()
+    return round(temperature, 1)
+ 
+for n in range(1, 20):
+    
+    temp = getTemp()
+    print temp
+    
+    sql = ("""INSERT INTO temp_mashing (timestamp, id, temperature) VALUES (CURRENT_TIMESTAMP,%s,%s)""",(id,temp))
+    
+    try:
+        print "Writing to database..."
+        # Execute the SQL command
+        cur.execute(*sql)
+        # Commit your changes in the database
+        db.commit()
+        print "Write OK"
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+        print "Failed writing to database"
+
+    time.sleep(interval)
+    
+cur.close()
+db.close()
