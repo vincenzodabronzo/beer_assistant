112796eb159f536678524b5407b3715e891bf7e0
vince <vince@DESKTOP-T101K01>
2017-11-15 19:11:37 +0100

    0.1.0a

--- Beer_Assistant/py/deactivate_heat.py
+++ Beer_Assistant/py/deactivate_heat.py
@@ -8,10 +8,12 @@ pinList = [17, 27]
 
 # loop through pins and set mode and state to 'low'
 
-GPIO.setup(17, GPIO.OUT) 
+#GPIO.setup(17, GPIO.OUT) 
 GPIO.output(17, GPIO.HIGH)
-GPIO.setup(27, GPIO.OUT) 
-GPIO.output(27, GPIO.HIGH)
+#GPIO.setup(22, GPIO.OUT) 
+GPIO.output(22, GPIO.HIGH)
 
 #GPIO.output(17, GPIO.LOW)
 #GPIO.output(27, GPIO.LOW)
+
+GPIO.cleanup()
