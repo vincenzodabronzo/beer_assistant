a49581a7d02868636e1e6cd1ac227d0c893e78d4
vince <vince@DESKTOP-T101K01>
2017-11-21 20:35:52 +0100

    0.1.0a

--- Beer_Assistant/py/mashtune_heat.py
+++ Beer_Assistant/py/mashtune_heat.py
@@ -17,7 +17,7 @@ heat = 0        # if set to 1, controller will activate heating
 GPIO.setmode(GPIO.BCM)
 pinList = [19, 20, 21, 26]
 # loop through pins and set mode and state to 'low'
-for i in init.pinList: 
+for i in pinList: 
     GPIO.setup(i, GPIO.OUT) 
     GPIO.output(i, GPIO.LOW)
 
