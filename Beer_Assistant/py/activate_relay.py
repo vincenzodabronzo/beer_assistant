5a93d87c15ae9b1d8f5378d1204f73414ddff06a
vince <vince@DESKTOP-T101K01>
2017-11-21 17:49:31 +0100

    0.1.0a

--- Beer_Assistant/py/activate_relay.py
+++ Beer_Assistant/py/activate_relay.py
@@ -1,12 +1,9 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-
 pinList = [19, 20, 21, 26]
 
 #sleep(0.5)
 
 # loop through pins and set mode and state to 'low'
 for i in pinList: 
-    GPIO.setup(i, GPIO.OUT) 
-    GPIO.output(i, GPIO.HIGH)
+    GPIO.output(i, GPIO.LOW)
