af88d39f0132e599c9b77f2ac07b85b8850b1332
vince <vince@DESKTOP-T101K01>
2017-11-21 17:53:56 +0100

    0.1.0a

--- Beer_Assistant/py/deactivate_relay.py
+++ Beer_Assistant/py/deactivate_relay.py
@@ -8,4 +8,4 @@ pinList = [19, 20, 21, 26]
 # loop through pins and set mode and state to 'low'
 for i in pinList: 
     GPIO.setup(i, GPIO.OUT) 
-    GPIO.output(i, GPIO.LOW)
+    GPIO.output(i, GPIO.HIGH)
