dbaa3335d3d63ed9c0c7114900860ba8d2c89ca8
vince <vince@DESKTOP-T101K01>
2017-12-03 17:09:05 +0100

    0.1.0a

--- Beer_Assistant/lib/end_mashing.php
+++ Beer_Assistant/lib/end_mashing.php
@@ -10,7 +10,7 @@
 
    $dbconn->query("UPDATE mashing_config SET ending_time = CURRENT_TIMESTAMP WHERE mashing_config.id = ".$id.";");
    
-   $result = $dbconn->query("SELECT ba.id, ba.name, mt.temperature, mt.timestamp, mc.starting_time, mc.ending_time FROM batch AS ba INNER JOIN ba ON mt.id = ba.id WHERE ba.id=".$id." ORDER BY timestamp DESC LIMIT 1");
+   $result = $dbconn->query("SELECT ba.id, ba.name, mt.temperature, mt.timestamp, mc.starting_time, mc.ending_time FROM batch AS ba INNER JOIN mashing_temp as mt ON ba.id = mt.id INNER JOIN mashing_config AS mc ON mt.id = mc.id WHERE ba.id=".$id." ORDER BY timestamp DESC LIMIT 1");
    
    if ($result->num_rows > 0) {
        while ($row = $result->fetch_assoc()){
