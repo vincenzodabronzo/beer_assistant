e6e708debfe4bac1eac7ac2a9a4bc7d5d7588bdc
vince <vince@DESKTOP-T101K01>
2017-12-03 19:44:01 +0100

    0.1.0a

--- Beer_Assistant/lib/start_mashing.php
+++ Beer_Assistant/lib/start_mashing.php
@@ -1,41 +1,21 @@
 <?php
     ini_set('display_errors', 'On');
+    
+    $receipe_name = "nuovo nome";
 
     $dbconn =  new mysqli('localhost', 'pi', 'raspberry', 'dbeer');
     if($dbconn->connect_error) {
         die('Connection error: ' . $dbconn->connect_error);
     }
     
-    $dbconn->query("INSERT INTO batch(name) VALUES ('nuovo nome') " );
+    $dbconn->query("INSERT INTO batch(name) VALUES ('".$receipe_name."') " );
     
     $id = $mysqli->insert_id;
     
     $dbconn->query("INSERT INTO mashing_config(id, starting_time, pump_recirculation) VALUES ( '".$id."', CURRENT_TIMESTAMP, '0' ) " );
+ 
     
-    
-    $result = $dbconn->query("SELECT id FROM batch WHERE mc.ending_time is NULL ORDER BY mc.id DESC LIMIT 1");
-    
-    if ($result->num_rows > 0) {
-        if ($row = $result->fetch_assoc()){
-            echo    '(Receipe name)<div id="batch_id">'. $row['id'] .'</div> ';
-            // shell_exec("python py/mashtune_heat.py > /dev/null 2>/dev/null &");
-        }
-    } else {
-        echo    '(No open batch found - Start mashing)<div id="batch_id">0</div> ';
-    }
-
-    
-    
-    
-    $result = $dbconn->query("SELECT mc.id FROM mashing_config AS mc WHERE mc.ending_time is NULL ORDER BY mc.id DESC LIMIT 1");
-    
-    if ($result->num_rows > 0) {
-        if ($row = $result->fetch_assoc()){
-            echo    '(Receipe name)<div id="batch_id">'. $row['id'] .'</div> ';
-            // shell_exec("python py/mashtune_heat.py > /dev/null 2>/dev/null &");
-        }
-    } else {
-        echo    '(No open batch found - Start mashing)<div id="batch_id">0</div> ';
-    }
+    echo    '(Receipe name)<div id="batch_id">'. $id .'</div> ';
+    // shell_exec("python py/mashtune_heat.py > /dev/null 2>/dev/null &");
 
 ?>
