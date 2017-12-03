d298dab1980e2b3e9f7d203f92b8db4cf3d48367
vince <vince@DESKTOP-T101K01>
2017-12-03 19:09:05 +0100

    0.1.0a

--- Beer_Assistant/lib/get_open_batch.php
+++ Beer_Assistant/lib/get_open_batch.php
@@ -11,6 +11,7 @@ $result = $dbconn->query("SELECT mc.id FROM mashing_config AS mc  WHERE mc.endin
 if ($result->num_rows > 0) {
     if ($row = $result->fetch_assoc()){
         echo    '(Receipe name)<div id="batch_id">'. $row['id'] .'</div> ';
+        // shell_exec("python py/mashtune_heat.py > /dev/null 2>/dev/null &");
     } 
 } else {
     echo    '(No open batched found - Start mashing)<div id="batch_id">0</div> ';
