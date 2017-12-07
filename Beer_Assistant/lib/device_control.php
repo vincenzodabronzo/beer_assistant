5c1343bb844ee34a2a4b8d83ec32675ae364b5aa
vince <vince@DESKTOP-T101K01>
2017-12-07 17:18:56 +0100

    0.1.0a

--- Beer_Assistant/lib/device_control.php
+++ Beer_Assistant/lib/device_control.php
@@ -5,13 +5,13 @@
 $command = $_GET["command"];
 $id = $_GET["id"];
 $step = $_GET["step"];
-$device_name = $_GET["device_name"];
+$device = $_GET["device"];
 
 $dbconn =  new mysqli('localhost', 'pi', 'raspberry', 'dbeer');
 if($dbconn->connect_error) {
     die('Connection error: ' . $dbconn->connect_error);
 }
 
-$dbconn->query("UPDATE ".$step."_config SET ".$device_name." = ".$command." WHERE ".$step."_config.id = ".$id.";");
+$dbconn->query("UPDATE ".$step."_config SET ".$device." = ".$command." WHERE ".$step."_config.id = ".$id.";");
 
 ?>
