6e1a17fbe614793c115da6f6c43e0680c7203bfa
vince <vince@DESKTOP-T101K01>
2017-12-07 18:12:48 +0100

    0.1.0a

--- Beer_Assistant/lib/start_fermentation.php
+++ Beer_Assistant/lib/start_fermentation.php
@@ -1,7 +1,7 @@
 <?php
     // ini_set('display_errors', 'On');
     
-$receipe_name = $_GET["receipe_name"];
+    $receipe_name = $_GET["receipe_name"];
     $upper_limit = $_GET["upper_limit"];
     $lower_limit = $_GET["lower_limit"];
 
@@ -15,7 +15,7 @@ $receipe_name = $_GET["receipe_name"];
     $id = $dbconn->insert_id;
     
     $dbconn->query("INSERT INTO fermentation_config(id, starting_time) VALUES ( '".$id."', CURRENT_TIMESTAMP ) " );
-    $dbconn->query("INSERT INTO fermentation_step(id, temp_min, temp_max, step_number) VALUES ( '".$id."', '".$lower_limit."', '1' ) " );
+    $dbconn->query("INSERT INTO fermentation_step(id, temp_min, temp_max, step_number) VALUES ( '".$id."', '".$lower_limit."', '".$upper_limit."', '1' ) " );
     
     echo    '(Receipe name)<div id="batch_id">'. $id .'</div> ';
     
