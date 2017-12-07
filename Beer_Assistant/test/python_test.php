7f072da460321b567890e6f3d45fb60a4d0b8e2e
vince <vince@DESKTOP-T101K01>
2017-12-07 17:07:40 +0100

    0.1.0a

--- Beer_Assistant/test/python_test.php
+++ Beer_Assistant/test/python_test.php
@@ -1,4 +1,6 @@
 <?php
-    echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py 2>&1");
+    // echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py 2>&1");
+    
+echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/fermentation_control.py 2>&1");
     
     ?>
