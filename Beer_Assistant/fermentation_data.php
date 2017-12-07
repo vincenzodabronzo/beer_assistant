18cb37268f04b95bc902d805f91e7cdeb82a33a7
vince <vince@DESKTOP-T101K01>
2017-12-07 18:33:03 +0100

    0.1.0a

--- Beer_Assistant/fermentation_data.php
+++ Beer_Assistant/fermentation_data.php
@@ -220,7 +220,7 @@
         	<fieldset id="receipe_info">
                     <legend>Receipe details: </legend>
                     <label for="receipe_name">Receipe name</label>
-                    <input type="text" name="receipe_info" id="receipe_name" maxlength="255"><br>         
+                    <input type="text" name="receipe_info" id="receipe_name" maxlength="255" value="insert receipe name"><br>         
 			</fieldset>
         </div>
     </div>
