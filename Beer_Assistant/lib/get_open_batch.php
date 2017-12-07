c846b400ac5f704fc3e153f080560cab92ca2d84
vince <vince@DESKTOP-T101K01>
2017-12-07 18:30:19 +0100

    0.1.0a

--- Beer_Assistant/lib/get_open_batch.php
+++ Beer_Assistant/lib/get_open_batch.php
@@ -10,7 +10,7 @@ if($dbconn->connect_error) {
     die('Connection error: ' . $dbconn->connect_error);
 }
 
-$result = $dbconn->query("SELECT c.id FROM ".$step."_config AS c WHERE c.ending_time is NULL ORDER BY c.id DESC LIMIT 1");
+$result = $dbconn->query("SELECT c.id, ba.name FROM ".$step."_config AS c INNER JOIN batch AS ba ON ba.id = c.id WHERE c.ending_time is NULL ORDER BY c.id DESC LIMIT 1");
 
 if ($result->num_rows > 0) {
     if ($row = $result->fetch_assoc()){
