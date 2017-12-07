19fd41268123c02b7e665197ecee57e769099409
vince <vince@DESKTOP-T101K01>
2017-12-07 13:09:43 +0100

    0.1.0a

--- Beer_Assistant/test/python_test.php
+++ Beer_Assistant/test/python_test.php
@@ -1,4 +1,4 @@
 <?php
-    echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py > /dev/null 2>&1");
+    echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py 2>&1");
     
     ?>
