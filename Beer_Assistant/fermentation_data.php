aab13584ae7f719c1ed4a52a1c7a20c733e0454c
vince <vince@DESKTOP-T101K01>
2017-12-07 14:46:21 +0100

    0.1.0a

--- Beer_Assistant/fermentation_data.php
+++ Beer_Assistant/fermentation_data.php
@@ -1,152 +1,152 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->
-<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
-
-<link href="css/fermentation_data.css" rel="stylesheet" type="text/css">
-<link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
-
-<script type="text/javascript" src="js/jquery.min.js"></script>
-
-<script type="text/javascript" src="js/jquery-ui.min.js"></script>
-
-<script type="text/javascript" src="js/moment.min.js"></script>
-<script type="text/javascript" src="js/moment-with-locales.min.js"></script>
-
-<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
-<script type="text/javascript" src="js/jqplot.dateAxisRenderer.js"></script>
-<script type="text/javascript" src="js/jqplot.pointLabels.js"></script>
-<script type="text/javascript" src="js/jqplot.canvasAxisLabelRenderer.js"></script>
-<script type="text/javascript" src="js/jqplot.canvasTextRenderer.js"></script>
-<script type="text/javascript" src="js/jqplot.canvasAxisTickRenderer.js"></script>
-<script type="text/javascript" src="js/jqplot.canvasOverlay.js"></script>
-<script type="text/javascript" src="js/jqplot.highlighter.js"></script>
-<script type="text/javascript" src="js/jqplot.cursor.js"></script>
-
-<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
-
-
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-<script type="text/javascript">
-
-//  microsec interval
-var t = 1000;
-
-
-$(document).ready(function() {
+	<meta charset="UTF-8">
+	<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->
+    <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
     
-    // Check for open batch (if yes, collect graph data and hide "start mashing")
-    $('#batch_title').load( 'lib/get_open_batch.php' );
+     <link href="css/mash_data.css" rel="stylesheet" type="text/css">
+     <link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
     
-    // RADIO BUTTON HANDLING  ------------------------------
-    $('#heat_auto').click(function () {
-        if ($(this).is(':checked')) {
-            $.ajax( "lib/heat_control.php?c=NULL&id="+$('#batch_id').text() )
-        }
-    });
-        $('#heat_on').click(function () {
-            if ($(this).is(':checked')) {
-                $.ajax( "lib/heat_control.php?c=1&id="+$('#batch_id').text() )
-            }
-        });
-            $('#heat_off').click(function () {
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    
+    <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+    
+    <script type="text/javascript" src="js/moment.min.js"></script>
+    <script type="text/javascript" src="js/moment-with-locales.min.js"></script>
+    
+    <script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
+    <script type="text/javascript" src="js/jqplot.dateAxisRenderer.js"></script>
+    <script type="text/javascript" src="js/jqplot.pointLabels.js"></script>
+    <script type="text/javascript" src="js/jqplot.canvasAxisLabelRenderer.js"></script>
+    <script type="text/javascript" src="js/jqplot.canvasTextRenderer.js"></script>
+    <script type="text/javascript" src="js/jqplot.canvasAxisTickRenderer.js"></script>
+    <script type="text/javascript" src="js/jqplot.canvasOverlay.js"></script>
+    <script type="text/javascript" src="js/jqplot.highlighter.js"></script>
+	<script type="text/javascript" src="js/jqplot.cursor.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
+	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+    
+    <script type="text/javascript">
+
+    	//  microsec interval
+    	var t = 1000;
+    	
+		
+		$(document).ready(function() {
+
+			// Check for open batch (if yes, collect graph data and hide "start mashing")
+			$('#batch_title').load( 'lib/get_open_batch.php' );
+
+			// RADIO BUTTON HANDLING  ------------------------------
+			$('#heat_auto').click(function () {
+                if ($(this).is(':checked')) {
+                    $.ajax( "lib/heat_control.php?c=NULL&id="+$('#batch_id').text() )
+                }
+   			});
+    		$('#heat_on').click(function () {
+                if ($(this).is(':checked')) {
+                    $.ajax( "lib/heat_control.php?c=1&id="+$('#batch_id').text() )
+                }
+    		});
+    		$('#heat_off').click(function () {
                 if ($(this).is(':checked')) {
                     $.ajax( "lib/heat_control.php?c=0&id="+$('#batch_id').text() )
                 }
-            });
-                
-                $('#pump_on').click(function () {
-                    if ($(this).is(':checked')) {
-                        $.ajax( "lib/pump_control.php?c=1&id="+$('#batch_id').text() )
-                    }
-                });
-                    $('#pump_off').click(function () {
-                        if ($(this).is(':checked')) {
-                            $.ajax( "lib/pump_control.php?c=0&id="+$('#batch_id').text() )
-                        }
-                    });
-                        
-                        // END RADIO BUTTON HANDLING ------------------------------
-                        
-                        
-                        var x = (new Date()).getTime(); // current time
-                        
-                        var n = 20;
-                        data = [];
-                        
-                        for(i=0; i<n; i++){
-                            data.push([x - (n-1-i)*t, 0]);
-                        }
-                        
-                        var options = {
-                            axes: {
-                                xaxis: {
-                                    tickRenderer:$.jqplot.CanvasAxisTickRenderer,
-                                    numberTicks: 10,
-                                    renderer:$.jqplot.DateAxisRenderer,
-                                    tickOptions:{
-                                        formatString:'%H:%M:%S',
-                                        // //labelPosition: 'middle',
-                                        angle:-30
-                                    },
-                                    min : data[0][0],
-                                    max: data[data.length-1][0],
-                                    label:'Time'
-                                },
-                                yaxis: {
-                                    labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
-                                    min: -10,
-                                    max: 120,
-                                    numberTicks: 14,
-                                    tickOptions:{formatString:'%.1f'},
-                                    label:'Temperature Celsius'
-                                }
-                            },
-                            seriesDefaults: {
-                                pointLabels: {
-                                    show: true
-                                },
-                                rendererOptions: { smooth: true}
-                            },
-                            highlighter: {
-                                show: true,
-                                sizeAdjust: 7.5
-                            },
-                            cursor: {
-                                show: false
-                            }
-                        };
-                        
-                        var plot1 = $.jqplot ('myChart', [data],options);
-                        
-                        $('#start').click( function(){
-                            $('#batch_title').load( 'lib/start_mashing.php' );
-                            location.reload();
-                            // doUpdate();
-                            
-                            $(this).hide();
-                            $('#stop').show();
-                        });
-                            
-                            $('#stop').click( function(){
-                                update_graph = false;
-                                endMashing();
-                                $(this).hide();
-                                $('#start').show();
-                            });
-                                
-                                function endMashing() {
-                                    $('#show_data').load('lib/data.php?id='+$('#batch_id').text()+'&end_mash=1' );
-                                    location.reload();
-                                }
-                                
-                                function doUpdate() {
-                                    
-                                    <?php shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py > /dev/null 2>/dev/null &"); ?>
+    		});
+    		
+    		$('#pump_on').click(function () {
+                if ($(this).is(':checked')) {
+                    $.ajax( "lib/pump_control.php?c=1&id="+$('#batch_id').text() )
+                }
+    		});
+    		$('#pump_off').click(function () {
+                if ($(this).is(':checked')) {
+                    $.ajax( "lib/pump_control.php?c=0&id="+$('#batch_id').text() )
+                }
+    		});
+    		
+    		// END RADIO BUTTON HANDLING ------------------------------
+    		
+			
+			var x = (new Date()).getTime(); // current time
+			
+			var n = 20;
+			data = [];
+			
+			for(i=0; i<n; i++){  
+			    data.push([x - (n-1-i)*t, 0]);  
+			}   
+			
+			var options = {      
+			      axes: {   	    
+			         xaxis: {     	   
+			        	tickRenderer:$.jqplot.CanvasAxisTickRenderer,
+				        numberTicks: 10,            
+			            renderer:$.jqplot.DateAxisRenderer,           
+			            tickOptions:{
+				            	formatString:'%H:%M:%S',
+				            	// //labelPosition: 'middle', 
+				                angle:-30
+						},            
+			            min : data[0][0],           
+			            max: data[data.length-1][0],
+			            label:'Time'	   
+					}, 	    
+					yaxis: {
+						labelRenderer: $.jqplot.CanvasAxisLabelRenderer,
+			            min: -10, 
+			            max: 120,
+			            numberTicks: 14,   	        
+			            tickOptions:{formatString:'%.1f'},
+						label:'Temperature Celsius'   
+					}      
+			      },      
+			      seriesDefaults: {   	    
+			    	  pointLabels: {
+		                    show: true
+		                },
+				         rendererOptions: { smooth: true}      
+			      },
+			      highlighter: {
+			          show: true,
+			          sizeAdjust: 7.5
+			        },
+			        cursor: {
+			          show: false
+			        }
+			  };  
+			 
+			   var plot1 = $.jqplot ('myChart', [data],options); 
+			 
+				$('#start').click( function(){
+					$('#batch_title').load( 'lib/start_mashing.php' );
+					location.reload();
+					// doUpdate();
+					     
+					$(this).hide();
+					$('#stop').show();
+			   });
+				   
+				$('#stop').click( function(){
+					update_graph = false;      
+					endMashing(); 
+					$(this).hide();
+					$('#start').show();
+				});
+
+				function endMashing() {
+					$('#show_data').load('lib/data.php?id='+$('#batch_id').text()+'&end_mash=1' );
+					location.reload();
+				}
+			 
+				function doUpdate() {
+
+	                    <?php shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/mashtune_heat.py > /dev/null 2>/dev/null &"); ?> 
 
 						
 						$('#show_data').load('lib/data.php?id='+$('#batch_id').text());
