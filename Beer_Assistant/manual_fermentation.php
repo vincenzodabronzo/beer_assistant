ad0eec91aed1e372efc5c08bd776a3bdb5c88ba7
vince <vince@DESKTOP-T101K01>
2017-12-08 22:58:09 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -10,10 +10,8 @@
     
      <link href="css/manual_fermentation.css" rel="stylesheet" type="text/css">
      <link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
-     <link href="css/switchery.min.css" rel="stylesheet" type="text/css">
      <link href="css/gauge.css" rel="stylesheet" type="text/css">
      
-     <script type="text/javascript" src="js/switchery.min.js"></script>
      <script type="text/javascript" src="js/raphael-2.1.4.min.js"></script>
      <script type="text/javascript" src="js/justgage.js"></script>
      <script type="text/javascript" src="js/jquery.min.js"></script>
