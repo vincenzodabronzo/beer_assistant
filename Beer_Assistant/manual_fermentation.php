14e2571866e5ec515cc672d4f125a7d203fa1e13
vince <vince@DESKTOP-T101K01>
2017-12-08 14:42:54 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -23,8 +23,10 @@
     <script type="text/javascript" src="js/jquery-ui.min.js"></script>
     <script type="text/javascript" src="js/moment.min.js"></script>
     <script type="text/javascript" src="js/moment-with-locales.min.js"></script>
+    
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
 	    
     <script type="text/javascript">
 
