7b321cf8d8f27e03338b7456c313d3175124c760
vince <vince@DESKTOP-T101K01>
2017-12-08 12:17:17 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -8,7 +8,7 @@
 	<!--[if lt IE 9]><script src="/js/html5shiv.js"></script><![endif]-->
     <!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]-->
     
-     <link href="css/fermentation_data.css" rel="stylesheet" type="text/css">
+     <link href="css/manual_fermentation.css" rel="stylesheet" type="text/css">
      <link href="css/jquery-ui.css" rel="stylesheet" type="text/css">
     
     <script type="text/javascript" src="js/jquery.min.js"></script>
