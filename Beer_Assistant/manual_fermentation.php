fabebf77470f2391b174d23a169c43fd11f65952
vince <vince@DESKTOP-T101K01>
2017-12-09 17:37:57 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -312,7 +312,23 @@
     <div id="starting_time">--</div>
  </div>
  
-
+<div class="control-group">
+    <label class="control control-radio" onclick="$.ajax( 'lib/device_control.php?command=NULL&id='+$('#batch_id').text()+'&step=fermentation&device=cooler' );">
+        Auto
+            <input type="radio" name="radio" checked="checked" />
+        <div class="control_indicator"></div>
+    </label>
+    <label class="control control-radio" onclick="$.ajax( 'lib/device_control.php?command=1&id='+$('#batch_id').text()+'&step=fermentation&device=cooler' );">
+        On
+            <input type="radio" name="radio" />
+        <div class="control_indicator"></div>
+    </label>
+    <label class="control control-radio" onclick="$.ajax( 'lib/device_control.php?command=0&id='+$('#batch_id').text()+'&step=fermentation&device=cooler' );">
+        Off
+            <input type="radio" name="radio" />
+        <div class="control_indicator"></div>
+    </label>
+</div>
 
 </body>
 
