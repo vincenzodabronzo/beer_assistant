2ec5133929d1c65a478a5275f37cc5b15a9376ee
vince <vince@DESKTOP-T101K01>
2017-12-10 11:04:12 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -242,8 +242,9 @@
 	
 	<div id="command">
 		<img id="play" src="img/play.png">
+		<img id="clock" src="img/clock.png">
 		<img id="options" src="img/options.png">
-		<img id="home" src="img/chart.png">
+		<img id="chart" src="img/chart.png">
 		<img id="help" src="img/help.png">
 	</div>
 	
