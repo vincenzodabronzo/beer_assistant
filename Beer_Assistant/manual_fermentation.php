82af613e87b39cb65438e800caa67db3b8e069b5
vince <vince@DESKTOP-T101K01>
2017-12-10 11:25:16 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -246,7 +246,6 @@
 		<img id="options" src="img/options.png">
 		<img id="chart" src="img/chart.png">
 		<img id="chart" src="img/share.png">
-		<img id="chart" src="img/cloud.png">
 		<img id="help" src="img/help.png">
 	</div>
 	
