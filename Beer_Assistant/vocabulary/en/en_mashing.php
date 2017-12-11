50595256c571f013a52eaeb9f5e26684da727b68
vince <vince@DESKTOP-T101K01>
2017-12-11 11:48:18 +0100

    0.1.0a

---
+++ Beer_Assistant/vocabulary/en/en_mashing.php
@@ -0,0 +1,17 @@
+<?php
+    $no_mashing_quote = array(   
+        "It appears that nothing is mashing... What a pity...",
+        "The sooner you start mashing, the better",
+        "I cannot drink but you can! Let's start fermentation!",
+    );
+    
+    $mashing_quote = array(
+        "Well done! Soon you'll have a good glass of beer",
+        "Shall we share this beer? Sharing is caring..."
+
+    );
+    
+    $mashing_info = "   <p>If temperature raises above the Target value spacified, the system will shut down the heater. It is possibile to override the auto option, specifying if the heater has to be activated or deactivated.</p>
+                <p>Furthermore, the Pump, if connected, can be swtched On and Off</p>";
+    
+?>
