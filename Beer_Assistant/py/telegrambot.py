6f2ce984e7f33b56a238ded4e027898fb2d16a68
vince <vince@DESKTOP-T101K01>
2017-12-11 19:32:16 +0100

    0.1.0a

--- Beer_Assistant/py/telegrambot.py
+++ Beer_Assistant/py/telegrambot.py
@@ -29,7 +29,7 @@ def on_chat_message(msg):
             bot.sendMessage(chat_id, '... I run out of jokes lately ...')
         elif command == 'bye':
             bot.sendMessage(chat_id, '... Goodbye, old friend. I\'ll go out and have a drink. See you soon')
-            sys.exit(0)
+            MessageLoop.cancel()
         else:
             content_type, chat_type, chat_id = telepot.glance(msg)
             bot.sendMessage(chat_id, 'mmm ... It\'s some kind of elvish... I can\' read it...')
@@ -60,8 +60,8 @@ bot = telepot.Bot(token)
 MessageLoop(bot, {'chat': on_chat_message, 'callback_query': on_callback_query}).run_as_thread()
 print('Listening ...')
  
-while 1:
-    time.sleep(10)
+#while 1:
+#    time.sleep(10)
     
 '''    
 bot.message_loop(handle)
