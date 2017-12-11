5105396b0ba226bb09c60256731d4ca1a5010ab2
vince <vince@DESKTOP-T101K01>
2017-12-11 12:10:21 +0100

    0.1.0a

--- Beer_Assistant/lib/get_open_batch.php
+++ Beer_Assistant/lib/get_open_batch.php
@@ -7,6 +7,8 @@ if($step=="fermentation") {
     include_once 'vocabulary/en/en_mashing.php';
 }
 
+$display = ' style="display: none;"';
+$display="";
 
 if($_GET["step"] != "") {
     $step = $_GET["step"];
@@ -23,18 +25,18 @@ if ($result->num_rows > 0) {
     if ($row = $result->fetch_assoc()){
         
         if($step=="fermentation") {
-            echo '<b>'.$fermentation_quote[array_rand($fermentation_quote)].'</b><br><br><div id="batch_id" style="display: none;">'. $row['id'] .'</div> ';
+            echo '<b>'.$fermentation_quote[array_rand($fermentation_quote)].'</b><br><br><div id="batch_id" '.$display.'>'. $row['id'] .'</div> ';
         } else if ($step=="mashing") {
-            echo '<b>'.$mashing_quote[array_rand($mashing_quote)].'</b><br><br><div id="batch_id" style="display: none;">'. $row['id'] .'</div> ';
+            echo '<b>'.$mashing_quote[array_rand($mashing_quote)].'</b><br><br><div id="batch_id" '.$display.'">'. $row['id'] .'</div> ';
         }
         
     } 
 } else {
     
     if($step=="fermentation") {
-        echo '<b>'.$no_fermentation_quote[array_rand($no_fermentation_quote)].'</b><br><br><div id="batch_id" style="display: none;">0</div> ';
+        echo '<b>'.$no_fermentation_quote[array_rand($no_fermentation_quote)].'</b><br><br><div id="batch_id" '.$display.'">0</div> ';
     } else if ($step=="mashing") {
-        echo '<b>'.$no_mashing_quote[array_rand($no_mashing_quote)].'</b><br><br><div id="batch_id" style="display: none;">'. $row['id'] .'</div> ';
+        echo '<b>'.$no_mashing_quote[array_rand($no_mashing_quote)].'</b><br><br><div id="batch_id" '.$display.'">'. $row['id'] .'</div> ';
     }
        
     
