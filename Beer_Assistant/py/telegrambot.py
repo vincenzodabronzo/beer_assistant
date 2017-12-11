423524ea7b3c799caaba6965de2edce08145804b
vince <vince@DESKTOP-T101K01>
2017-12-11 17:07:28 +0100

    0.1.0a

--- Beer_Assistant/py/telegrambot.py
+++ Beer_Assistant/py/telegrambot.py
@@ -1,5 +1,6 @@
 import json 
 import requests
+import urllib
 
 TOKEN = "458737458:AAHskrQVsMN32bBeexZcruDK3x9hz8vmhaY"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -17,10 +18,27 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
 
 
 def get_last_chat_id_and_text(updates):
@@ -30,11 +48,23 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
-
 def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
     
+    
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
 
 text, chat = get_last_chat_id_and_text(get_updates())
 send_message(text, chat)
