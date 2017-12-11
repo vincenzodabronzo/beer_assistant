9d2c3fd568fd345c70dd265ccac1dbc3fbb408ad
vince <vince@DESKTOP-T101K01>
2017-12-11 20:02:50 +0100

    0.1.0a

---
+++ Beer_Assistant/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+
+     <script type="text/javascript" src="js/jquery.min.js"></script>
+	
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title>Main menu</title>
+</head>
+
+<body>
+
+<div id="maincontainer">
+
+
+	
+	<div id="command">
+		<img id="play" src="img/manual_mashing.png">
+		<img id="play" src="img/manual_fermentation.png">
+		<img id="options" src="img/options.png">
+		<img id="share" src="img/share.png">
+		<img id="help" src="img/help.png">
+	</div>
+	
+	
+
+	<br>
+	<br>
+	
+</div>
+
+
+</body>
+
+</html>
