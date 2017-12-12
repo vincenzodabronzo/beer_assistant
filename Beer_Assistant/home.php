7fe3ee8c987d879d7d0765ba993085967fa98030
vince <vince@DESKTOP-T101K01>
2017-12-12 15:09:09 +0100

    0.1.0a

--- Beer_Assistant/home.php
+++ Beer_Assistant/home.php
@@ -79,7 +79,7 @@
                     <br><br>Authorized users:
                     <br>
                     <div id="authorized_users"></div>
-                    <div id="add_new_user"> Token: <input type="text" id="token"><br>User id:<input type="text" id="userid"><br><img src="img/add.png"></div>
+                    <div id="add_new_user"> <div>Token</div> <input type="text" id="token"><div>User id</div><input type="text" id="userid"><br><img src="img/add.png"></div>
                     
                     
                     
