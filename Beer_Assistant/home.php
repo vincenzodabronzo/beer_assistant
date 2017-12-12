4010c29737b31601f016ff218658578a4cefaddb
vince <vince@DESKTOP-T101K01>
2017-12-12 09:36:05 +0100

    0.1.0a

--- Beer_Assistant/home.php
+++ Beer_Assistant/home.php
@@ -17,8 +17,8 @@
 
 	
 	<div id="command">
-		<img id="play" src="img/manual_mashing.png">
-		<img id="play" src="img/manual_fermentation.png">
+		<img id="mashing" src="img/mashing_menu.jpg">
+		<img id="frementation" src="img/fermentation_menu.jpg">
 		<img id="options" src="img/options.png">
 		<img id="share" src="img/share.png">
 		<img id="help" src="img/help.png">
