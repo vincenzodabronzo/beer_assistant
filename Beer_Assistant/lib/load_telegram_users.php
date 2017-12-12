b4f5f07a5425f8d130de7be2096a8606b04f4963
vince <vince@DESKTOP-T101K01>
2017-12-12 16:18:56 +0100

    0.1.0a

--- Beer_Assistant/lib/load_telegram_users.php
+++ Beer_Assistant/lib/load_telegram_users.php
@@ -9,7 +9,12 @@ $result = $dbconn->query("SELECT tg.token, tg.user_id FROM system_config_telegra
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()){
-        echo    '<div id="'.$row["token"].$row["userid"].'"><div>&nbsp;Token</div><div>&nbsp;'.$row["token"].'</div><div>&nbsp;User Id</div><div>&nbsp;'.$row["user_id"].'</div><div>&nbsp;<img id="remove_user" src="img/remove.png"></div></div>';
+        echo    '<div id="'.$row["token"].$row["userid"].'"><div>&nbsp;Token</div><div>&nbsp;'.$row["token"].'</div><div>&nbsp;User Id</div><div>&nbsp;'.$row["user_id"].'</div><div>&nbsp;<img id="img'.$row["token"].$row["userid"].'" src="img/remove.png"></div></div>
+                    <script>
+                    	document.getElementById("img'.$row["token"].$row["userid"].'").addEventListener("click", function(){
+			             removeUser(\''.$row["token"].'\', \''.$row["userid"].'\');
+			             });
+                    </script>';
     }
 } else echo "(none)";
 
