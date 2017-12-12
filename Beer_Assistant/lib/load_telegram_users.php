dce32b32eb1c2bc8f5d0430924ce50030b5285e3
vince <vince@DESKTOP-T101K01>
2017-12-12 15:52:15 +0100

    0.1.0a

--- Beer_Assistant/lib/load_telegram_users.php
+++ Beer_Assistant/lib/load_telegram_users.php
@@ -9,7 +9,7 @@ $result = $dbconn->query("SELECT tg.token, tg.user_id FROM system_config_telegra
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()){
-        echo    '<div id="'.$row["token"].$row["userid"].'"><div>&nbsp;Token</div><div>&nbsp;'.$row["token"].'</div><div>&nbsp;User Id</div><div>&nbsp;'.$row["user_id"].'</div><div>&nbsp;<img id="remove_user" src="img/remove.png" onclick="removeUser(\''.$row["token"].'\',\''.$row["user_id"].'\');"></div></div>';
+        echo    '<div id="'.$row["token"].$row["userid"].'"><div>&nbsp;Token</div><div>&nbsp;'.$row["token"].'</div><div>&nbsp;User Id</div><div>&nbsp;'.$row["user_id"].'</div><div>&nbsp;<img id="remove_user" src="img/remove.png" onclick="javascript: removeUser(\''.$row["token"].'\',\''.$row["user_id"].'\');"></div></div>';
     }
 } else echo "(none)";
 
