867d7ae437ba34dd31c2a1514c170c86e302dda5
vince <vince@DESKTOP-T101K01>
2017-12-12 17:43:39 +0100

    0.1.0a

--- Beer_Assistant/home.php
+++ Beer_Assistant/home.php
@@ -8,7 +8,7 @@
 	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	 <link href="css/home.css" rel="stylesheet" type="text/css">
 	 
-	 <?php  ini_set('display_errors', 'On'); shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/telegrambot.py > /dev/null 2>/dev/null &"); ?>
+	 <?php  ini_set('display_errors', 'On'); echo shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/telegrambot.py 2>&1"); // shell_exec("python /var/www/html/beer_assistant/Beer_Assistant/py/telegrambot.py > /dev/null 2>/dev/null &"); ?>
 	 
 	 
 	     <script type="text/javascript">
