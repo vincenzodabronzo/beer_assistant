9a01daaca7db22458f933b9f244e0dc3bb9bf25a
vince <vince@DESKTOP-T101K01>
2017-12-12 17:04:29 +0100

    0.1.0a

--- Beer_Assistant/home.php
+++ Beer_Assistant/home.php
@@ -35,10 +35,10 @@
 			});
 
 			document.getElementById("add_new_user").addEventListener("click", function(){
-                // $.ajax( "lib/telegrambot_control.php?command=add&token='"+$('#token').val()+"'&userid='"+$('#userid').val()+"'");
+                $.ajax( "lib/telegrambot_control.php?command=add&token="+$('#token').val()+"&userid="+$('#userid').val()+);
 				// alert("lib/telegrambot_control.php?command=add&token="+$('#token').val()+"&userid="+$('#userid').val());
                 //location.reload();
-                alert($('#token').val()+"'&userid='"+$('#userid').val());
+                // alert($('#token').val()+"'&userid='"+$('#userid').val());
              });
 
 			
