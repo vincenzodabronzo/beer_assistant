ccae00fd6d93c2b1ad795086ceaa2a9ee3467ae2
vince <vince@DESKTOP-T101K01>
2017-12-13 15:37:03 +0100

    0.1.0a

--- Beer_Assistant/manual_fermentation.php
+++ Beer_Assistant/manual_fermentation.php
@@ -17,8 +17,11 @@
      <script type="text/javascript" src="js/justgage.js"></script>
      <script type="text/javascript" src="js/jquery.min.js"></script>
      <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+     
+      <!--
      <script type="text/javascript" src="js/moment.min.js"></script>
      <script type="text/javascript" src="js/moment-with-locales.min.js"></script>
+	-->
 	
 	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
