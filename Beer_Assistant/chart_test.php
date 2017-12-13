2a5d9129291ee811d975b76d4eca5bab46c3e279
vince <vince@DESKTOP-T101K01>
2017-12-13 16:20:19 +0100

    0.1.0a

--- Beer_Assistant/chart_test.php
+++ Beer_Assistant/chart_test.php
@@ -14,38 +14,19 @@
 		$(document).ready(function() {
 
 			var ctx = document.getElementById("myChart");
-			var myChart = new Chart(ctx, {
-			    type: 'bar',
-			    data: {
-			        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-			        datasets: [{
-			            label: '# of Votes',
-			            data: [12, 19, 3, 5, 2, 3],
-			            backgroundColor: [
-			                'rgba(255, 99, 132, 0.2)',
-			                'rgba(54, 162, 235, 0.2)',
-			                'rgba(255, 206, 86, 0.2)',
-			                'rgba(75, 192, 192, 0.2)',
-			                'rgba(153, 102, 255, 0.2)',
-			                'rgba(255, 159, 64, 0.2)'
-			            ],
-			            borderColor: [
-			                'rgba(255,99,132,1)',
-			                'rgba(54, 162, 235, 1)',
-			                'rgba(255, 206, 86, 1)',
-			                'rgba(75, 192, 192, 1)',
-			                'rgba(153, 102, 255, 1)',
-			                'rgba(255, 159, 64, 1)'
-			            ],
-			            borderWidth: 1
-			        }]
-			    },
+			var myLineChart = new Chart(ctx, {
+			    type: 'line',
+			    data: [{
+			        x: 10,
+			        y: 20
+			    }, {
+			        x: 15,
+			        y: 10
+			    }],
 			    options: {
 			        scales: {
 			            yAxes: [{
-			                ticks: {
-			                    beginAtZero:true
-			                }
+			                stacked: true
 			            }]
 			        }
 			    }
