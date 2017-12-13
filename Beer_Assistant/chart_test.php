b35165b0023c623bc8dd8c983807567785d5cd47
vince <vince@DESKTOP-T101K01>
2017-12-13 16:09:46 +0100

    0.1.0a

---
+++ Beer_Assistant/chart_test.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+
+     <script type="text/javascript" src="js/jquery.min.js"></script>
+     <script type="text/javascript" src="js/chart.bundle.min.js"></script>
+	
+	 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 <link href="css/home.css" rel="stylesheet" type="text/css">	 
+	 
+	     <script type="text/javascript">
+
+		$(document).ready(function() {
+
+			var ctx = $("#myChart");
+			
+			new Chart(ctx, {
+			    type: 'line',
+			    data: data,
+			    options: {
+			        elements: {
+			            line: {
+			                tension: 0, // disables bezier curves
+			            }
+			        }
+			    }
+			});
+
+			
+			
+	});
+</script>
+	
+	<title>Chart</title>
+</head>
+
+<body>
+
+	<canvas id="myChart" width="400" height="400"></canvas>
+
+
+	
+</body>
+
+</html>
