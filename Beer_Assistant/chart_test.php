a921a650158fc0015270bdf0693a37c8bb874c84
vince <vince@DESKTOP-T101K01>
2017-12-13 16:54:10 +0100

    0.1.0a

--- Beer_Assistant/chart_test.php
+++ Beer_Assistant/chart_test.php
@@ -13,45 +13,7 @@
 
 		$(document).ready(function() {
 
-			new Chart(document.getElementById("line-chart"), {
-				  type: 'line',
-				  data: {
-				    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
-				    datasets: [{ 
-				        data: [86,114,106,106,107,111,133,221,783,2478],
-				        label: "Africa",
-				        borderColor: "#3e95cd",
-				        fill: true
-				      }, { 
-				        data: [282,350,411,502,635,809,947,1402,3700,5267],
-				        label: "Asia",
-				        borderColor: "#8e5ea2",
-				        fill: true
-				      }, { 
-				        data: [168,170,178,190,203,276,408,547,675,734],
-				        label: "Europe",
-				        borderColor: "#3cba9f",
-				        fill: true
-				      }, { 
-				        data: [40,20,10,16,24,38,74,167,508,784],
-				        label: "Latin America",
-				        borderColor: "#e8c3b9",
-				        fill: true
-				      }, { 
-				        data: [6,3,2,2,7,26,82,172,312,433],
-				        label: "North America",
-				        borderColor: "#c45850",
-				        fill: true
-				      }
-				    ]
-				  },
-				  options: {
-				    title: {
-				      display: true,
-				      text: 'World population per region (in millions)'
-				    }
-				  }
-				});
+			new Chart(document.getElementById("line-chart"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});
 			
 			
 	});
