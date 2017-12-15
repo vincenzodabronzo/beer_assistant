d19a07e95bc8f551aefb564c5ae5b38bb11c49b8
vince <vince@DESKTOP-T101K01>
2017-12-15 13:45:51 +0100

    0.1.0a

--- Beer_Assistant/py/multiprobe.py
+++ Beer_Assistant/py/multiprobe.py
@@ -18,3 +18,5 @@ for i in pinList:
 for sensor in W1ThermSensor.get_available_sensors([W1ThermSensor.THERM_SENSOR_DS18B20]):
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
+print "Cleaning up GPIO"
+GPIO.cleanup()
