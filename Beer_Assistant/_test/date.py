012eb1b860ed3bc2fbe0503014124516730cee88
vince <vince@DESKTOP-T101K01>
2018-01-09 17:31:08 +0100

    0.1.0a

--- Beer_Assistant/_test/date.py
+++ Beer_Assistant/_test/date.py
@@ -3,7 +3,7 @@ import datetime
 mysql = '2013-01-12 15:27:43'
 f = '%Y-%m-%d %H:%M:%S'
 
-datetime.datetime.strptime(mysql, f)
+print datetime.datetime.strptime(mysql, f)
 
 now = datetime.datetime.now()
 
